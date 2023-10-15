93b6ed9fc0213c1dccb3bcd9af328aa9cf0e3666
Varuna Priya@gmail.com <you@example.com>
2023-10-15 15:07:16 +0530

    modified:   example/package.json modified:   example/src/App.js modified:   example/src/App.js

--- example/src/App.js
+++ example/src/App.js
@@ -7,6 +7,7 @@ import Form from './Components/Form';
 //import StyleSheets from './Components/StyleSheets';
 //import NameList from './Components/NameList';
 //import UserGreeting from './Components/UserGreeting';
+
 //import ParentComponent from './Components/ParentComponent';
 //import EventBind from './Components/EventBind'
 //import ClassClick from './Components/ClassClick';
